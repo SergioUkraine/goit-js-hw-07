f292119010eca5a086fd1dda8edfc9899bc950ff
Serhii <38428786+SergioUkraine@users.noreply.github.com>
2023-04-24 22:45:15 +0300

    1sr step

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryCards = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryCards);
+
+galleryContainer.addEventListener('click', handleGalleryClick);
+
+function createGalleryMarkup(pictures) {
+    return pictures
+        .map(({ preview, original, description }) => {
+            return `
+            <li class="gallery__item">
+               <a class="gallery__link" href="${original}">
+                  <img class="gallery__image" src="${preview}" alt="${description}" />
+               </a>
+            </li>`; 
+        })
+        .join(" ");
+}
+
+function handleGalleryClick(e) {
+    e.preventDefault();
+    if (e.target.tagName !== 'IMG') return;
+    lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+}
