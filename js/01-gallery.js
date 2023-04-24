10ff091ac4ffe65fb2a80bf36762dfb4fdb114b6
Serhii <38428786+SergioUkraine@users.noreply.github.com>
2023-04-24 18:37:56 +0300

    addition feature

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+
 const galeryContainer = document.querySelector('.gallery');
 const galleryCards = createGalleryMarkup(galleryItems);
 galeryContainer.insertAdjacentHTML('beforeend', galleryCards);
@@ -7,14 +8,20 @@ galeryContainer.insertAdjacentHTML('beforeend', galleryCards);
 galeryContainer.addEventListener('click', handleClickElementGallety);
 
 
+
 function handleClickElementGallety(event) {
     if (event.target.tagName !== 'IMG') {
         return;
     }
+    galeryContainer.addEventListener('keydown', (event) => {
+        if (event.key !== 'Escape') {
+            return;
+        }
+        instance.close();
+    })
 
     const instance = basicLightbox.create(`<img src="${event.target.dataset.source}" width="800" height="600">`);
-
-    instance.show()
+    instance.show();
 }
 
 function createGalleryMarkup(pictures) {
