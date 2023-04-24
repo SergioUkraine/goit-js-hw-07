d2e64ae418f9322cf380a6db30f8715d30f03e57
Serhii <38428786+SergioUkraine@users.noreply.github.com>
2023-04-24 17:25:04 +0300

    task1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galeryContainer = document.querySelector('.gallery');
+const galleryCards = createGalleryMarkup(galleryItems);
+galeryContainer.insertAdjacentHTML('beforeend', galleryCards);
 
-console.log(galleryItems);
+galeryContainer.addEventListener('click', handleClickElementGallety);
+
+
+function handleClickElementGallety(event) {
+    if (event.target.tagName !== 'IMG') {
+        return;
+    }
+
+    const instance = basicLightbox.create(`<img src="${event.target.dataset.source}" width="800" height="600">`);
+
+    instance.show()
+}
+
+function createGalleryMarkup(pictures) {
+    return pictures
+        .map(({ preview, original, description }) => {
+            return `
+            <li class="gallery__item">
+                <a class="gallery__link" href="#">
+                  <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                  />
+              </a>
+            </li>
+            `;
+        })
+        .join(" ");
+}
