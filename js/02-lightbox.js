6753ec2d6eb96f7ac970b749eeed7a1d0e1d6897
Serhii <38428786+SergioUkraine@users.noreply.github.com>
2023-04-25 21:34:16 +0300

    task2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,23 +5,26 @@ const galleryContainer = document.querySelector('.gallery');
 const galleryCards = createGalleryMarkup(galleryItems);
 galleryContainer.insertAdjacentHTML('beforeend', galleryCards);
 
-galleryContainer.addEventListener('click', handleGalleryClick);
-
 function createGalleryMarkup(pictures) {
     return pictures
         .map(({ preview, original, description }) => {
             return `
             <li class="gallery__item">
-               <a class="gallery__link" href="${original}">
-                  <img class="gallery__image" src="${preview}" alt="${description}" />
-               </a>
-            </li>`; 
+                <a class="gallery__link" href="${original}">
+                    <img 
+                        class="gallery__image" 
+                        src="${preview}" 
+                        alt="${description}" 
+                    />
+                </a>
+            </li>
+            `; 
         })
-        .join(" ");
+        .join("");
 }
 
-function handleGalleryClick(e) {
-    e.preventDefault();
-    if (e.target.tagName !== 'IMG') return;
-    lightbox = new SimpleLightbox('.gallery a', { /* options */ });
-}
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    captionPosition: "bottom",
+});
